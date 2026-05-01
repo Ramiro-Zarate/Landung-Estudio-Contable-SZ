0e60ef2ebe67d5aef986e56895794fb3aa8b36c2
Ramiro-Zarate <ramazara13@gmail.com>
2026-05-01 11:27:45 -0300

    refactor: Nav style

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,10 +3,15 @@ import styles from './Hero.module.css'
 export function Hero(){
     return(
         <section className={styles.hero}>
-            <h2 className={styles.heroTitle}> 
-                Bienvenido a Estudio Contable SZ, tu socio confiable para soluciones contables y financieras. 
-            </h2>
-            <p className={styles.heroDesc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere omnis quaerat consequuntur obcaecati incidunt voluptates dolor, voluptatem temporibus ut natus accusantium placeat deserunt possimus dignissimos laboriosam beatae corrupti. Earum, sequi.</p>
+            <div className={styles.heroContent}>
+                <h2 className={styles.heroTitle}>
+                    Bienvenido a Estudio Contable SZ, tu socio confiable para soluciones contables y financieras.
+                </h2>
+                <p className={styles.heroDesc}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere omnis quaerat consequuntur obcaecati incidunt voluptates dolor, voluptatem temporibus ut natus accusantium placeat deserunt possimus dignissimos laboriosam beatae corrupti. Earum, sequi.
+                </p>
+                <button className={styles.heroBtn}>Conocer Más</button>
+            </div>
         </section>
     )
 }
